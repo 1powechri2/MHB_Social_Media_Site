7a73a7b45fea8d6386931be94c861fdb498ffda4
Chris Powell <chris.ian.powell@gmail.com>
2018-12-02 17:05:04 -0700

    Adds migrations, tests and the model for mile high bibliophiles users

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :bio, presence: true
+  validates :mhb_pics, presence: true
+  validates :email, presence: true
+  has_secure_password
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:bio) }
+  it { should validate_presence_of(:mhb_pics) }
+  it { should validate_presence_of(:email) }
+  it { should have_secure_password }
+end
+
+describe 'A user' do
+  it 'has attributes' do
+    user = {first_name: 'Chris',
+           last_name: 'Powell',
+           username: 'booklover420',
+           bio: 'I love stuff, fun haha.',
+           mhb_pics: 'Blackass',
+           email: 'chris.ian.powell@gmail.com',
+           password: 'sfyack1'}
+    me = User.create(user)
+
+    expect(me.first_name).to eq(user[:first_name])
+    expect(me.password).to eq(user[:password])
+  end
+end

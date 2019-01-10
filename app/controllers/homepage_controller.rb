97db6da73bf1ac4e996468c8c53ea1c0c210e0ad
Chris Powell <chris.ian.powell@gmail.com>
2019-01-10 15:29:12 -0700

    Adds change to sign up form

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,6 +2,7 @@ class HomepageController < ApplicationController
   def index
     @posts = Post.all
     @post  = Post.new
+    @user  = User.new
     @books = Book.where(:status => 'reading')
   end
 end
